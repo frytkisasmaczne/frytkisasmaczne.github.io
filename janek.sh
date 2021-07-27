cc44ed67555671564ae4270c9053462885f1890d
frytkisasmaczne <frytkisasmaczne@gmail.com>
2021-07-27 21:28:19 +0200

    fghd

--- janek.sh
+++ janek.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir ~/ngrok
+cd ~/ngrok
 curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ngrok.zip
 unzip ngrok.zip
 ./ngrok authtoken 7R6WoSgxiHibE2zqYYQG8_3dTEHMctioMv28dC4n1bV
